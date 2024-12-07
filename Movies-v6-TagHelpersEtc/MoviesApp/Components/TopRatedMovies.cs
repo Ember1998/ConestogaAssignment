3b1f2bf5b5ab886238c3384d13415bbcd894b8b4
Sahil Man Singh <sahil.singh@javra.com>
2024-12-07 21:06:46 +0545

    ConestogaAssignment added

---
+++ Movies-v6-TagHelpersEtc/MoviesApp/Components/TopRatedMovies.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using MoviesApp.DataAccess;
+
+namespace MoviesApp.Components
+{
+    public class TopRatedMovies : ViewComponent
+    {
+        // Just like controllers, the DI container can inject DB
+        // context objects/service objects into out components if needed:
+        public TopRatedMovies(MovieDbContext movieDbContext)
+        {
+            _movieDbContext = movieDbContext;
+        }
+
+        public IViewComponentResult Invoke(int lowestRating, int numberOfMoviesToDisplay)
+        {
+            // use DB context to get movies with avg rating >= lowestRating:
+            var movies = _movieDbContext.Movies
+                    .Include(m => m.Genre)
+                    .Include(m => m.Reviews)
+                    .Where(m => m.Reviews.Average(r => r.Rating).GetValueOrDefault() >= lowestRating)
+                    .OrderBy(m => m.Name)
+                    .ToList();
+
+            TopRatedMoviesViewModel topRatedMoviesViewModel = new TopRatedMoviesViewModel() { 
+                Movies = movies,
+                LowestRating = lowestRating,
+                NumberOfMoviesToDisplay = numberOfMoviesToDisplay
+            };
+
+            return View(topRatedMoviesViewModel);
+        }
+
+        private MovieDbContext _movieDbContext;
+    }
+}
